2fab2a0e39e087c2b3db27ad9bffba2f0596c072
johnnyhotshot <johnnypuskar@yahoo.com>
2018-11-20 01:20:19 -0500

    Notification text for date entry and probably some other stuff

--- TimeMachine/Form1.Designer.cs
+++ TimeMachine/Form1.Designer.cs
@@ -36,21 +36,20 @@
 			this.lastTime = new System.Windows.Forms.Label();
 			this.pad1 = new System.Windows.Forms.Button();
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.pad2 = new System.Windows.Forms.Button();
-			this.pad3 = new System.Windows.Forms.Button();
-			this.pad4 = new System.Windows.Forms.Button();
-			this.pad5 = new System.Windows.Forms.Button();
-			this.pad6 = new System.Windows.Forms.Button();
-			this.pad7 = new System.Windows.Forms.Button();
-			this.pad8 = new System.Windows.Forms.Button();
-			this.pad9 = new System.Windows.Forms.Button();
-			this.pad0 = new System.Windows.Forms.Button();
-			this.padConfirm = new System.Windows.Forms.Button();
-			this.padCancel = new System.Windows.Forms.Button();
 			this.padGo = new System.Windows.Forms.Button();
+			this.padCancel = new System.Windows.Forms.Button();
+			this.padConfirm = new System.Windows.Forms.Button();
+			this.pad0 = new System.Windows.Forms.Button();
+			this.pad9 = new System.Windows.Forms.Button();
+			this.pad8 = new System.Windows.Forms.Button();
+			this.pad7 = new System.Windows.Forms.Button();
+			this.pad6 = new System.Windows.Forms.Button();
+			this.pad5 = new System.Windows.Forms.Button();
+			this.pad4 = new System.Windows.Forms.Button();
+			this.pad3 = new System.Windows.Forms.Button();
+			this.pad2 = new System.Windows.Forms.Button();
 			this.controlOutput = new System.Windows.Forms.RichTextBox();
-			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.mainText = new System.Windows.Forms.RichTextBox();
 			this.currentTimeBox.SuspendLayout();
 			this.destinationTimeBox.SuspendLayout();
 			this.lastTimeBox.SuspendLayout();
@@ -155,105 +154,15 @@
 			this.panel1.Size = new System.Drawing.Size(164, 144);
 			this.panel1.TabIndex = 4;
 			// 
-			// pad2
+			// padGo
 			// 
-			this.pad2.Location = new System.Drawing.Point(39, 3);
-			this.pad2.Name = "pad2";
-			this.pad2.Size = new System.Drawing.Size(30, 30);
-			this.pad2.TabIndex = 4;
-			this.pad2.Text = "2";
-			this.pad2.UseVisualStyleBackColor = true;
-			this.pad2.Click += new System.EventHandler(this.pad2_Click);
-			// 
-			// pad3
-			// 
-			this.pad3.Location = new System.Drawing.Point(75, 3);
-			this.pad3.Name = "pad3";
-			this.pad3.Size = new System.Drawing.Size(30, 30);
-			this.pad3.TabIndex = 5;
-			this.pad3.Text = "3";
-			this.pad3.UseVisualStyleBackColor = true;
-			this.pad3.Click += new System.EventHandler(this.pad3_Click);
-			// 
-			// pad4
-			// 
-			this.pad4.Location = new System.Drawing.Point(3, 39);
-			this.pad4.Name = "pad4";
-			this.pad4.Size = new System.Drawing.Size(30, 30);
-			this.pad4.TabIndex = 5;
-			this.pad4.Text = "4";
-			this.pad4.UseVisualStyleBackColor = true;
-			this.pad4.Click += new System.EventHandler(this.pad4_Click);
-			// 
-			// pad5
-			// 
-			this.pad5.Location = new System.Drawing.Point(39, 39);
-			this.pad5.Name = "pad5";
-			this.pad5.Size = new System.Drawing.Size(30, 30);
-			this.pad5.TabIndex = 5;
-			this.pad5.Text = "5";
-			this.pad5.UseVisualStyleBackColor = true;
-			this.pad5.Click += new System.EventHandler(this.pad5_Click);
-			// 
-			// pad6
-			// 
-			this.pad6.Location = new System.Drawing.Point(75, 39);
-			this.pad6.Name = "pad6";
-			this.pad6.Size = new System.Drawing.Size(30, 30);
-			this.pad6.TabIndex = 5;
-			this.pad6.Text = "6";
-			this.pad6.UseVisualStyleBackColor = true;
-			this.pad6.Click += new System.EventHandler(this.pad6_Click);
-			// 
-			// pad7
-			// 
-			this.pad7.Location = new System.Drawing.Point(3, 75);
-			this.pad7.Name = "pad7";
-			this.pad7.Size = new System.Drawing.Size(30, 30);
-			this.pad7.TabIndex = 5;
-			this.pad7.Text = "7";
-			this.pad7.UseVisualStyleBackColor = true;
-			this.pad7.Click += new System.EventHandler(this.pad7_Click);
-			// 
-			// pad8
-			// 
-			this.pad8.Location = new System.Drawing.Point(39, 75);
-			this.pad8.Name = "pad8";
-			this.pad8.Size = new System.Drawing.Size(30, 30);
-			this.pad8.TabIndex = 5;
-			this.pad8.Text = "8";
-			this.pad8.UseVisualStyleBackColor = true;
-			this.pad8.Click += new System.EventHandler(this.pad8_Click);
-			// 
-			// pad9
-			// 
-			this.pad9.Location = new System.Drawing.Point(75, 75);
-			this.pad9.Name = "pad9";
-			this.pad9.Size = new System.Drawing.Size(30, 30);
-			this.pad9.TabIndex = 5;
-			this.pad9.Text = "9";
-			this.pad9.UseVisualStyleBackColor = true;
-			this.pad9.Click += new System.EventHandler(this.pad9_Click);
-			// 
-			// pad0
-			// 
-			this.pad0.Location = new System.Drawing.Point(39, 111);
-			this.pad0.Name = "pad0";
-			this.pad0.Size = new System.Drawing.Size(30, 30);
-			this.pad0.TabIndex = 5;
-			this.pad0.Text = "0";
-			this.pad0.UseVisualStyleBackColor = true;
-			this.pad0.Click += new System.EventHandler(this.pad0_Click);
-			// 
-			// padConfirm
-			// 
-			this.padConfirm.BackColor = System.Drawing.Color.ForestGreen;
-			this.padConfirm.Location = new System.Drawing.Point(121, 3);
-			this.padConfirm.Name = "padConfirm";
-			this.padConfirm.Size = new System.Drawing.Size(40, 30);
-			this.padConfirm.TabIndex = 5;
-			this.padConfirm.UseVisualStyleBackColor = false;
-			this.padConfirm.Click += new System.EventHandler(this.padConfirm_Click);
+			this.padGo.BackColor = System.Drawing.Color.CadetBlue;
+			this.padGo.Location = new System.Drawing.Point(121, 75);
+			this.padGo.Name = "padGo";
+			this.padGo.Size = new System.Drawing.Size(40, 66);
+			this.padGo.TabIndex = 6;
+			this.padGo.UseVisualStyleBackColor = false;
+			this.padGo.Click += new System.EventHandler(this.padGo_Click);
 			// 
 			// padCancel
 			// 
@@ -265,51 +174,134 @@
 			this.padCancel.UseVisualStyleBackColor = false;
 			this.padCancel.Click += new System.EventHandler(this.padCancel_Click);
 			// 
-			// padGo
+			// padConfirm
 			// 
-			this.padGo.BackColor = System.Drawing.Color.CadetBlue;
-			this.padGo.Location = new System.Drawing.Point(121, 75);
-			this.padGo.Name = "padGo";
-			this.padGo.Size = new System.Drawing.Size(40, 66);
-			this.padGo.TabIndex = 6;
-			this.padGo.UseVisualStyleBackColor = false;
-			this.padGo.Click += new System.EventHandler(this.padGo_Click);
+			this.padConfirm.BackColor = System.Drawing.Color.ForestGreen;
+			this.padConfirm.Location = new System.Drawing.Point(121, 3);
+			this.padConfirm.Name = "padConfirm";
+			this.padConfirm.Size = new System.Drawing.Size(40, 30);
+			this.padConfirm.TabIndex = 5;
+			this.padConfirm.UseVisualStyleBackColor = false;
+			this.padConfirm.Click += new System.EventHandler(this.padConfirm_Click);
+			// 
+			// pad0
+			// 
+			this.pad0.Location = new System.Drawing.Point(39, 111);
+			this.pad0.Name = "pad0";
+			this.pad0.Size = new System.Drawing.Size(30, 30);
+			this.pad0.TabIndex = 5;
+			this.pad0.Text = "0";
+			this.pad0.UseVisualStyleBackColor = true;
+			this.pad0.Click += new System.EventHandler(this.pad0_Click);
+			// 
+			// pad9
+			// 
+			this.pad9.Location = new System.Drawing.Point(75, 75);
+			this.pad9.Name = "pad9";
+			this.pad9.Size = new System.Drawing.Size(30, 30);
+			this.pad9.TabIndex = 5;
+			this.pad9.Text = "9";
+			this.pad9.UseVisualStyleBackColor = true;
+			this.pad9.Click += new System.EventHandler(this.pad9_Click);
+			// 
+			// pad8
+			// 
+			this.pad8.Location = new System.Drawing.Point(39, 75);
+			this.pad8.Name = "pad8";
+			this.pad8.Size = new System.Drawing.Size(30, 30);
+			this.pad8.TabIndex = 5;
+			this.pad8.Text = "8";
+			this.pad8.UseVisualStyleBackColor = true;
+			this.pad8.Click += new System.EventHandler(this.pad8_Click);
+			// 
+			// pad7
+			// 
+			this.pad7.Location = new System.Drawing.Point(3, 75);
+			this.pad7.Name = "pad7";
+			this.pad7.Size = new System.Drawing.Size(30, 30);
+			this.pad7.TabIndex = 5;
+			this.pad7.Text = "7";
+			this.pad7.UseVisualStyleBackColor = true;
+			this.pad7.Click += new System.EventHandler(this.pad7_Click);
+			// 
+			// pad6
+			// 
+			this.pad6.Location = new System.Drawing.Point(75, 39);
+			this.pad6.Name = "pad6";
+			this.pad6.Size = new System.Drawing.Size(30, 30);
+			this.pad6.TabIndex = 5;
+			this.pad6.Text = "6";
+			this.pad6.UseVisualStyleBackColor = true;
+			this.pad6.Click += new System.EventHandler(this.pad6_Click);
+			// 
+			// pad5
+			// 
+			this.pad5.Location = new System.Drawing.Point(39, 39);
+			this.pad5.Name = "pad5";
+			this.pad5.Size = new System.Drawing.Size(30, 30);
+			this.pad5.TabIndex = 5;
+			this.pad5.Text = "5";
+			this.pad5.UseVisualStyleBackColor = true;
+			this.pad5.Click += new System.EventHandler(this.pad5_Click);
+			// 
+			// pad4
+			// 
+			this.pad4.Location = new System.Drawing.Point(3, 39);
+			this.pad4.Name = "pad4";
+			this.pad4.Size = new System.Drawing.Size(30, 30);
+			this.pad4.TabIndex = 5;
+			this.pad4.Text = "4";
+			this.pad4.UseVisualStyleBackColor = true;
+			this.pad4.Click += new System.EventHandler(this.pad4_Click);
+			// 
+			// pad3
+			// 
+			this.pad3.Location = new System.Drawing.Point(75, 3);
+			this.pad3.Name = "pad3";
+			this.pad3.Size = new System.Drawing.Size(30, 30);
+			this.pad3.TabIndex = 5;
+			this.pad3.Text = "3";
+			this.pad3.UseVisualStyleBackColor = true;
+			this.pad3.Click += new System.EventHandler(this.pad3_Click);
+			// 
+			// pad2
+			// 
+			this.pad2.Location = new System.Drawing.Point(39, 3);
+			this.pad2.Name = "pad2";
+			this.pad2.Size = new System.Drawing.Size(30, 30);
+			this.pad2.TabIndex = 4;
+			this.pad2.Text = "2";
+			this.pad2.UseVisualStyleBackColor = true;
+			this.pad2.Click += new System.EventHandler(this.pad2_Click);
 			// 
 			// controlOutput
 			// 
 			this.controlOutput.Location = new System.Drawing.Point(174, 225);
 			this.controlOutput.Name = "controlOutput";
 			this.controlOutput.ReadOnly = true;
+			this.controlOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
 			this.controlOutput.Size = new System.Drawing.Size(135, 144);
 			this.controlOutput.TabIndex = 5;
 			this.controlOutput.Text = "";
 			// 
-			// richTextBox1
+			// mainText
 			// 
-			this.richTextBox1.Font = new System.Drawing.Font("Consolas", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.richTextBox1.Location = new System.Drawing.Point(322, 18);
-			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(454, 312);
-			this.richTextBox1.TabIndex = 6;
-			this.richTextBox1.Text = "";
-			// 
-			// richTextBox2
-			// 
-			this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.richTextBox2.Location = new System.Drawing.Point(322, 340);
-			this.richTextBox2.Multiline = false;
-			this.richTextBox2.Name = "richTextBox2";
-			this.richTextBox2.Size = new System.Drawing.Size(454, 29);
-			this.richTextBox2.TabIndex = 7;
-			this.richTextBox2.Text = "command goes here";
+			this.mainText.BackColor = System.Drawing.SystemColors.Desktop;
+			this.mainText.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.mainText.ForeColor = System.Drawing.SystemColors.HighlightText;
+			this.mainText.Location = new System.Drawing.Point(322, 18);
+			this.mainText.Name = "mainText";
+			this.mainText.ReadOnly = true;
+			this.mainText.Size = new System.Drawing.Size(454, 454);
+			this.mainText.TabIndex = 6;
+			this.mainText.Text = "";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(784, 380);
-			this.Controls.Add(this.richTextBox2);
-			this.Controls.Add(this.richTextBox1);
+			this.ClientSize = new System.Drawing.Size(784, 526);
+			this.Controls.Add(this.mainText);
 			this.Controls.Add(this.controlOutput);
 			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.lastTimeBox);
@@ -348,8 +340,7 @@
 		private System.Windows.Forms.Button pad3;
 		private System.Windows.Forms.Button pad2;
 		private System.Windows.Forms.RichTextBox controlOutput;
-		private System.Windows.Forms.RichTextBox richTextBox1;
-		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.RichTextBox mainText;
 	}
 }
 
--- TimeMachine/Form1.cs
+++ TimeMachine/Form1.cs
@@ -12,7 +12,7 @@ namespace TimeMachine
 {
 	public partial class Form1 : Form
 	{
-		// Timecode Format MMDDYYYYTTTT (Day, Month, Year, Time)
+		// Timecode Format MMDDYYYYTTTT (Month, Day, Year, Time)
 		String[] months = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
 		int[] monthDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
@@ -21,23 +21,26 @@ namespace TimeMachine
 		String currentTimecode = "";
 		String lastTimecode = "";
 
+		int seed = 100;
+		Random rng;
+		Tile[,] map = new Tile[40, 40];
 
 		public long getTimestamp(string timecode)
 		{
 			// 010100000000
 			long returnValue = 0;
-			if (timecode.Length != 12) { Console.WriteLine("Incorrect Code Length");  return -1; }
+			if (timecode.Length != 12) { Console.WriteLine("Incorrect Code Length"); return -1; }
 			long month = Convert.ToInt64(timecode.Substring(0, 2));
 			long days = Convert.ToInt64(timecode.Substring(2, 2));
 			long year = Convert.ToInt64(timecode.Substring(4, 4));
 			long hours = Convert.ToInt64(timecode.Substring(8, 2));
 			long minutes = Convert.ToInt64(timecode.Substring(10, 2));
 
-			if (month <= 0 || month > 12) { Console.WriteLine("Month invalid: " + month);  return -1; }
-			if (days <= 0 || days > getDayCount(Convert.ToInt32(month) - 1, Convert.ToInt32(year))) { Console.WriteLine("Day invalid: " + days);  return -1; }
+			if (month <= 0 || month > 12) { Console.WriteLine("Month invalid: " + month); return -1; }
+			if (days <= 0 || days > getDayCount(Convert.ToInt32(month) - 1, Convert.ToInt32(year))) { Console.WriteLine("Day invalid: " + days); return -1; }
 
-			if (hours < 0 || hours >= 24) { Console.WriteLine("Hour invalid: " + hours);  return -1; }
-			if (minutes < 0 || minutes >= 60) { Console.WriteLine("Minute invalid: " + minutes);  return -1; }
+			if (hours < 0 || hours >= 24) { Console.WriteLine("Hour invalid: " + hours); return -1; }
+			if (minutes < 0 || minutes >= 60) { Console.WriteLine("Minute invalid: " + minutes); return -1; }
 			returnValue += minutes + (hours * 60) + (getDaysPast(month, days, year) * 24 * 60);
 			return returnValue;
 		}
@@ -74,8 +77,8 @@ namespace TimeMachine
 				int month = Convert.ToInt32(timecode.Substring(0, 2)) - 1;
 				if (month < 0 || month >= 12) { return error; }
 				int days = Convert.ToInt32(timecode.Substring(2, 2));
-				if (days <= 0 || days > getDayCount(month+1, -1)) { return error; }
-				int time = Convert.ToInt32(timecode.Substring(8,4));
+				if (days <= 0 || days > getDayCount(month + 1, -1)) { return error; }
+				int time = Convert.ToInt32(timecode.Substring(8, 4));
 				if (time >= 2400 || time % 100 >= 60) { return error; }
 				String AMorPM = "AM";
 				String zeroesLOL = "";
@@ -140,8 +143,14 @@ namespace TimeMachine
 			lastTime.Text = getTimeString(lastTimecode);
 		}
 
+		public void displayMessage(String message)
+		{
+			controlOutput.Text = message + "\n" + controlOutput.Text;
+		}
+
 		public Form1()
 		{
+			rng = new Random(seed);
 			InitializeComponent();
 		}
 
@@ -210,7 +219,7 @@ namespace TimeMachine
 
 		private void pad0_Click(object sender, EventArgs e)
 		{
-			if (codeInput.Length >= 12) { return; } 
+			if (codeInput.Length >= 12) { return; }
 			codeInput += "0";
 			updateDisplay();
 		}
@@ -222,16 +231,105 @@ namespace TimeMachine
 				destinationTimecode = codeInput;
 				codeInput = "";
 				updateDisplay();
+				displayMessage("Destination time set!");
 			}
 		}
 
 		private void padCancel_Click(object sender, EventArgs e)
 		{
-			codeInput = "";
+			if (codeInput != "")
+			{
+				codeInput = "";
+				displayMessage("Input cancelled.");
+			}
 			updateDisplay();
 		}
 
 		private void padGo_Click(object sender, EventArgs e)
+		{
+			lastTimecode = currentTimecode;
+			currentTimecode = destinationTimecode;
+			updateDisplay();
+			//renderMap();
+		}
+
+		public Tile[,] generateMap(int width, int height)
+		{
+			Tile[,] toReturn = new Tile[width, height];
+			for (int i = 0; i < width; i++)
+			{
+				for (int j = 0; j < height; j++)
+				{
+					toReturn[i, j] = new Tile(rng.Next(0,80000)/100000d, rng.Next(0, 60000) / 100000d);
+				}
+			}
+			return toReturn;
+		}
+
+		public void processMap(Tile[,] world)
+		{
+			for (int x = 0; x < world.GetLength(0); x++)
+			{
+				for (int y = 0; y < world.GetLength(1); y++)
+				{
+					for (int i = x - 1; i <= x + 1; i++)
+					{
+						for (int j = y - 1; j <= y + 1; j++)
+						{
+							if(i >= 0 && i < world.GetLength(0) && j >= 0 && j < world.GetLength(1) && (i != x && j != y))
+							{
+								// Code run using individual tiles (world[i, j] is each surrounding tile)
+
+							}
+						}
+					}
+					// Code run after analyzing surrounding tiles
+
+				}
+			}
+		}
+
+		public void renderMap()
+		{
+			mainText.Text = "";
+			for (int i = 0; i < map.Length; i++)
+			{
+				mainText.Text += "$";
+				for (int j = 1; j < map.Length; j++)
+				{
+					//mainText.Text += map[i, j].representation;
+				}
+				mainText.Text += "\n";
+			}
+		}
+	}
+
+	public class Tile
+	{
+		public String representation;
+
+		// 0.0 - 1.0 scales for what is on the tile
+		public double water;
+		// Creates low amounts of animal life, decreases with urbanization
+		public double plantLife;
+		// Increase with nearby water and plant life, decreases with nearby animals/low/mid-tech urbanization
+		public double animalLife;
+		// Increase with nearby plant life (water but to a lesser degree), decreses with nearby humans/urbanization
+		public double humanLife;
+		// Increases with nearby animals and water, decreases with high urbanization combined with low tech for long
+		//  Increases urbanization and tech level on tile
+		//  Decreases plants and animals on all nearby tiles
+
+		public double urbanization;
+		// How much city/industry is on the tile, larger means more structure and less wilderness
+		public double technologyLevel;
+		// How advanced the city is, tech advances slowly and makes cities more attractive
+		// Higher technology reduces need costs for humans
+		public double livability = 1.0;
+		// How well the tile sustains life, lower livability reduces animals and humans, decresed by urbanization and mid tech
+		//  Increased very slowly by plants
+
+		public Tile(double water, double plant)
 		{
 
 		}
